be94e7511317340c9bf2b2baeb21b58358d6f6fd
zbrinck <zackbrincken@gmail.com>
2026-02-16 17:34:45 -0600

    refresh

--- records/models.py
+++ records/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
+from django.contrib.auth.models import User
 from .utils import load_county_choices
+
+
+#####################################
+#          PERSON TABLES            #
+#####################################
+
 
 COUNTIES = load_county_choices()
 
@@ -245,3 +252,27 @@ class Marriage(models.Model):
         if self.spouse2.id < self.spouse1.id:
             self.spouse1, self.spouse2 = self.spouse2, self.spouse1
         super().save(*args, **kwargs)
+
+
+
+#################################
+#         COMMENT MODELS        #
+#################################
+
+
+
+class Comment(models.Model):
+
+    # metadata
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+        ordering = ['-creation_time']
+
+    # comment content
+    comment_content = models.CharField(max_length=2000)
+    creation_time = models.DateTimeField()
+
+    # user optional content
+    commenter_name = models.CharField(max_length=100, blank=True, null=True)
+    commenter_email = models.CharField(max_length=100, blank=True, null=True)
